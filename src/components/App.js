488a3299e27c78ca053eb9140cdb7c7371cdcade
AryanGarate <aryangarate9152@gmail.com>
2022-11-09 00:29:10 +0530

    Add CSS file

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainApp";
+import { MainApp } from "./components/MainApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>
+);
